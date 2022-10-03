1944a7426ab9887277f81d693c368a01e395c76c
Yasin Quraishy <yasin.quraishy@gmail.com>
2022-10-03 12:53:08 +0530

    Change setup done

--- website-frontend/vue.config.js
+++ website-frontend/vue.config.js
@@ -1,9 +1,31 @@
 // SPDX-License-Identifier: Apache-2.0
+const path = require('path')
 module.exports = {
   transpileDependencies: ['vuetify'],
   configureWebpack: {
     resolve: {
       symlinks: false, //npm link
-    }
+    },
+  },
+  chainWebpack: config => {
+    config.module
+      .rule('supportchaining')
+      .test(/\.js$/)
+      .include.add(
+        path.resolve(
+          __dirname,
+          'node_modules/@google/model-viewer/node_modules/three/examples/jsm/exporters/GLTFExporter.js',
+        ),
+      )
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          plugins: ['@babel/plugin-proposal-optional-chaining'],
+        };
+      })
+      .end();
   },
 };
